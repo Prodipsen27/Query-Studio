23e423968fcafa5f1a3b4dff212b2a749f5524de
Prodip Sengupta <122111565+Prodipsen27@users.noreply.github.com>
2026-04-08 04:46:11 +0530

    Update App.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -39,7 +39,7 @@ function App() {
 
     const fetchStats = async () => {
       try {
-        const res = await fetch('https://query-studio.vercel.app');
+        const res = await fetch('https://query-studio.vercel.app/api');
         const data = await res.json();
         setStats(data);
       } catch (e) {
